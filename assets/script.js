d365be2c275723346bc2bc83aea4eb2f562e0e62
harveyb4b <harveyb4b@yahoo.com>
2020-06-30 21:37:20 -0400

    Adding API result variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 
 
+
 var appId = "820ec0b8"
 var appKey = "04c431218d56d654849a5ee10439cbba"
 var queryURL = `https://api.edamam.com/search?app_id=${appId}&app_key=${appKey}&q=chicken`
@@ -11,7 +12,33 @@ $.ajax({
 })
     .then(function (response) {
 
-        console.log(response);
+    var recipeResults = response.hits;
+    for (var i = 0; i < recipeResults.length; i++) {
+        var recipeName = recipeResults[i].recipe.label; 
+        var numIngredients = recipeResults[i].recipe.ingredientLines.length;
+        var numServings = recipeResults[i].recipe.yield;
+        var caloriesPerServing = recipeResults[i].recipe.calories;
+        var timeToCook = recipeResults[i].recipe.totalTime; 
+        var dietType = recipeResults[i].recipe.dietLabels;    
+        var recipeURL = recipeResults[i].recipe.url;
+        var recipeImage = recipeResults[i].recipe.image;
+        console.log(recipeName)
+        console.log(numIngredients)
+        console.log(numServings)
+        console.log(caloriesPerServing)
+        console.log(timeToCook)
+        console.log(dietType)
+        console.log(recipeURL)
+        console.log(recipeImage)
+        console.log("......")
+
+
+
+    } 
+    
+    console.log(response)
+    
         
+
 });
 
